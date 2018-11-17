70ce237e3630dca2f570decf5ba1bcab98f8e116
Qianye(Renee) Mei <meiqianye@gmail.com>
2018-11-17 09:54:17 -0500

    add camera zoom

--- main.py
+++ main.py
@@ -2,21 +2,17 @@
 ###Author: Qianye(Renee) Mei
 
 '''
-The starter code is modified from dinoint's code from
+The code skeleton is modified from dinoint's code from
 https://discourse.panda3d.org/t/using-panda3d-with-pyqt-or-pygtk-
 possible/6170
 '''
 #PySide2 imports
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
-#from PySide2.QtCore.QtObject import *
 
 #Panda3D imports
-#import direct.directbase.DirectStart
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
-from direct.actor.Actor import Actor
-from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3, WindowProperties
 
 from pandac.PandaModules import loadPrcFileData
@@ -32,22 +28,36 @@ P3D_WIN_HEIGHT = 240
 
 class Model(object):
 
-    def __init__(self, path):
+    def __init__(self, path, model):
         self.path = path
+        self.model = model
 
-#The Main Program Window
+    def __repr__(self):
+        return "Model at " + self.path
+
+#The Main Program Window (GUI)
 class QTWindow(QWidget):
     def __init__(self, program = None):
         super().__init__()
+
+        #Refering to the backend program
         self.program = program
+        #Window properties
         self.setWindowTitle("Procedural Castle Generator")
-        self.setGeometry(0, 0, 400, 300)
-
+        self.setGeometry(0, 0, 800, 600)
+        #Set aside the space for Panda3D window
         self.pandaContainer = QTPandaWidget(self)
-
+        #side menu
         self.sideMenu = QWidget()
-
+        self.menulayout = QHBoxLayout()
+        #Generate initial buttons
         self.generateButtons()
+        #List of Models
+        self.modelLst = QWidget()
+        self.modelLstLayout = QVBoxLayout()
+        self.modelLst.setLayout(self.modelLstLayout)
+        self.menulayout.addWidget(self.modelLst)
+        self.sideMenu.setLayout(self.menulayout)
 
         layout = QGridLayout()
         layout.addWidget(self.pandaContainer, 0, 0)
@@ -58,11 +68,13 @@ class QTWindow(QWidget):
         self.setLayout(layout)
 
     def generateButtons(self):
-        layout = QVBoxLayout()
+        buttonWidget = QWidget()
+        lyt = QHBoxLayout()
         addModelButton = QPushButton("Add")
         addModelButton.clicked.connect(self.addModel)
-        layout.addWidget(addModelButton)
-        self.sideMenu.setLayout(layout)
+        lyt.addWidget(addModelButton)
+        buttonWidget.setLayout(lyt)
+        self.menulayout.addWidget(buttonWidget)
 
     #Add Model to the Scene
     def addModel(self):
@@ -74,7 +86,15 @@ class QTWindow(QWidget):
             print(fileName)
             self.program.addModel(fileName)
 
-
+    #Renew the List of Models on GUI
+    def addModelUI(self, model):
+        groupBox = QGroupBox(model.path.split("/")[-1])
+        isWall = QRadioButton("Wall")
+        isWall.setChecked(False)
+        vbox = QVBoxLayout()
+        vbox.addWidget(isWall)
+        groupBox.setLayout(vbox)
+        self.modelLstLayout.addWidget(groupBox)
 
 #The resizable widget holding the panda3D window
 class QTPandaWidget(QWidget):
@@ -110,10 +130,22 @@ class World(ShowBase):
         self.scene.setPos(0, 0, 0)
         self.taskMgr.add(self.setCameraTask, "SetCameraTask")
 
+        #Refering to the front-end
+        self.window = None
+
+        #Keyboard Events
+        self.accept("arrow_up-up", self.fn, ["forward", 0])
+        self.accept("wheel_up", self.zoom, [1])
+        self.accept("wheel_down", self.zoom, [-1])
+
+        #Mouse Events
+        self.mouseTask = taskMgr.add(self.mouseTask, "Mouse Task")
+
+
     #Set the initial camera position
     def setCameraTask(self, task):
-        self.camera.setPos(0, -15, 70)
-        self.camera.setHpr(0, -80, 0)
+        self.camera.setPos(50, -50, 90)
+        self.camera.setHpr(45, -50, 0)
         return Task.done
 
 
@@ -127,20 +159,42 @@ class World(ShowBase):
 
     def addModel(self, fileName):
         print("in add Model in ShowBase")
-        self.models.add(Model(fileName))
         model = self.loader.loadModel(fileName)
-        model.reparentTo(self.render)
-        model.setPos(-8, 42, 0)
+        model.setPos(0, 0, 0)
         model.setScale(2,2,2)
+        model.reparentTo(self.render)
+        modelObj = Model(fileName, model)
+        self.models.add(modelObj)
+        self.window.addModelUI(modelObj)
+
+    def fn(self, dir, num):
+        print("Keyboard Input!")
+
+    #Method to handle zoom event
+    def zoom(self, dir):
+        distance = base.camLens.getFocalLength()
+        if distance + dir > 0:
+            base.camLens.setFocalLength(distance + dir)
+
+    def mouseTask(self, task):
+        mw = self.mouseWatcherNode
+
+        hasMouse = mw.hasMouse()
+        if hasMouse:
+            x = base.mouseWatcherNode.getMouseX()
+            y = base.mouseWatcherNode.getMouseY()
+
+        return Task.cont
+
 
 
 
 
 if __name__ == '__main__':
     world = World()
-
     app = QApplication(sys.argv)
     form = QTWindow(world)
+    world.window = form
     world.bindToWindow(int(form.winId()))
     form.show()
     world.run()
